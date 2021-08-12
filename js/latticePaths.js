3ef7841418eb51ca251305caa285e9ca978abf22
James Sinkler <jamessinkler713@gmail.com>
2021-08-11 19:15:02 -0700

    js version of lattice paths recursion

---
+++ js/latticePaths.js
@@ -0,0 +1,17 @@
+function latticePaths(m, n) {
+    // Write your code here
+    function recursePaths(row,col) {
+    // Base cases
+        if (row>m || col>n) {
+            return 0
+        } else if (row == m && col ==n){
+            return 1
+        }
+        return recursePaths(row + 1, col) + recursePaths(row, col + 1)
+    }
+    return recursePaths(0, 0)
+}
+
+// INPUT (2,3)
+// Expected OUTPUT : 10
+console.log(latticePaths(2,3))
