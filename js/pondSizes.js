a6860e07063a03da67f35fe29974de8b1ab28d5f
James Sinkler <jamessinkler713@gmail.com>
2021-09-12 18:23:42 -0700

    set up of problem

---
+++ js/pondSizes.js
@@ -0,0 +1,36 @@
+/*
+ * Rectangular Integer matrix representing a plog of land where the values represent the height above pond level. Zero's correspond to the water. Connected zero's (vertically, horizontally, diagonally) represent a pond. Write a function to compute the corresponding sizes of all ponds.
+ * 
+ * EX: [
+ *     [0, 2, 1, 0],
+ *     [0, 1, 0, 1],
+ *     [1, 1, 0, 1],
+ *     [0, 1, 0, 1]
+ *     ]
+ *
+ * Output: [ 2, 4, 1]
+ *
+ *  Once we get a zero we want to be able to see if the pond extends
+ *  
+ *  We want to be able to check left/right up/down diag and see if we have a zero.
+ *
+ *  If we have a zero, we need to add to that tally of the that particular pondSize.
+ *
+ *  If we don't have another zero we need to terminate doing recursive calls, but
+ *  We should keep track of where we have been, so we don't double count
+ *
+ */
+const testLand =  [
+      [0, 2, 1, 0],
+      [0, 1, 0, 1],
+      [1, 1, 0, 1],
+      [0, 1, 0, 1]
+      ]
+
+function pondCount(land) {
+  // const maxRow = land.length;
+  // const maxCol = land[0].length;
+
+}
+
+console.log(pondCount(testLand))
