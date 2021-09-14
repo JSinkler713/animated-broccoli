ab7f02bd1a9746fb3f9bb3b72260b739e0a865b0
James Sinkler <jamessinkler713@gmail.com>
2021-09-13 17:17:19 -0700

    1/2 way? there

--- js/pondSizes.js
+++ js/pondSizes.js
@@ -19,7 +19,9 @@
  *  If we don't have another zero we need to terminate doing recursive calls, but
  *  We should keep track of where we have been, so we don't double count
  *
+ * 
  */
+
 const testLand =  [
       [0, 2, 1, 0],
       [0, 1, 0, 1],
@@ -28,8 +30,42 @@ const testLand =  [
       ]
 
 function pondCount(land) {
-  // const maxRow = land.length;
-  // const maxCol = land[0].length;
+  const maxRow = land.length - 1
+  const maxCol = land[0].length - 1
+  const visitedPlaces = {}
+  // of the form {row_col: true}
+  function recursionHelper(row, col, thisPond) {
+    // base cases
+      // out of bounds
+    if ( row < 0 || col < 0 || row > maxRow || col > maxCol) {
+      return
+    }
+    // is not a non-zero
+    if (land[row][col] !== 0) {
+      // don't recurse we will hit it by iterating through the matrix
+      return
+    } else {
+      // it is a water
+      // check if in visitedPlaces
+      if (!visitedPlaces[row+'_'+col]) {
+        visitedPlaces[row+'_'+col] = true
+      }
+    // recursive call
+    recursionHelper(row + 1, col + 1, thisPond)
+    recursionHelper(row - 1, col - 1, thisPond)
+    recursionHelper(row + 1, col - 1, thisPond)
+    recursionHelper(row - 1, col + 1, thisPond)
+    recursionHelper(row - 1, col, thisPond)
+    recursionHelper(row + 1, col, thisPond)
+    recursionHelper(row, col + 1, thisPond)
+    recursionHelper(row, col - 1, thisPond)
+    }
+
+    // add row col to current pond
+    
+  }
+  recursionHelper(0, 0, {})
+  console.log(visitedPlaces)
 
 }
 
