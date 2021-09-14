717f85b7c98a09debad0d907a82f718c91594c21
James Sinkler <jamessinkler713@gmail.com>
2021-09-14 08:05:18 -0700

    3/4 way there

--- js/pondSizes.js
+++ js/pondSizes.js
@@ -19,6 +19,7 @@
  *  If we don't have another zero we need to terminate doing recursive calls, but
  *  We should keep track of where we have been, so we don't double count
  *
+ *
  * 
  */
 
@@ -33,39 +34,41 @@ function pondCount(land) {
   const maxRow = land.length - 1
   const maxCol = land[0].length - 1
   const visitedPlaces = {}
+  const pondSizes = []
   // of the form {row_col: true}
-  function recursionHelper(row, col, thisPond) {
-    // base cases
-      // out of bounds
-    if ( row < 0 || col < 0 || row > maxRow || col > maxCol) {
+  function recursionHelper(row, col) {
+    // base case
+    // out of bounds or not 0 or visited
+    if ( row < 0 || col < 0 || row > maxRow || col > maxCol || land[row][col] !== 0 || visitedPlaces[row+'_'+col] == true) {
       return
     }
-    // is not a non-zero
-    if (land[row][col] !== 0) {
-      // don't recurse we will hit it by iterating through the matrix
-      return
-    } else {
-      // it is a water
-      // check if in visitedPlaces
-      if (!visitedPlaces[row+'_'+col]) {
-        visitedPlaces[row+'_'+col] = true
-      }
-    // recursive call
-    recursionHelper(row + 1, col + 1, thisPond)
-    recursionHelper(row - 1, col - 1, thisPond)
-    recursionHelper(row + 1, col - 1, thisPond)
-    recursionHelper(row - 1, col + 1, thisPond)
-    recursionHelper(row - 1, col, thisPond)
-    recursionHelper(row + 1, col, thisPond)
-    recursionHelper(row, col + 1, thisPond)
-    recursionHelper(row, col - 1, thisPond)
-    }
-
-    // add row col to current pond
+    console.log('in recursion helper', row, col)
     
+    let pondSize = 1; // we have a 0
+    // mark as seen
+    visitedPlaces[row+'_'+col] = true
+    // recursive calls
+    pondSize += recursionHelper(row + 1, col + 1)
+    pondSize += recursionHelper(row - 1, col - 1)
+    pondSize += recursionHelper(row + 1, col - 1)
+    pondSize += recursionHelper(row - 1, col + 1)
+    pondSize += recursionHelper(row - 1, col)
+    pondSize += recursionHelper(row + 1, col)
+    pondSize += recursionHelper(row, col + 1)
+    pondSize += recursionHelper(row, col - 1)
+
+    return pondSize
   }
-  recursionHelper(0, 0, {})
+  // call for each element, since we are keeping track of visited it shouldn't get too large
+  for (let i=0; i<= maxRow; i++) {
+    for (let j=0; j<= maxCol; j++) {
+      console.log('i and j', i, j)
+      let pondSize = recursionHelper(i, j)
+      pondSizes.push(pondSize)
+    }
+  }
   console.log(visitedPlaces)
+  return pondSizes
 
 }
 
