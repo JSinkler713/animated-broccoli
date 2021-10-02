9a6a21e0f86bc36c5b04046dd095e39d1b3125c9
James Sinkler <jamessinkler713@gmail.com>
2021-10-01 18:10:51 -0700

    count to a million, side notes

--- js/setFromArray.js
+++ js/setFromArray.js
@@ -5,3 +5,9 @@ const newSet = new Set(coolArr)
 
 // {1, 3, 2} {1, 3, 2}.size = 3
 console.log('the number of distinct integers', newSet.size)
+
+console.time("How long does it take to count to a million")
+
+for (let i=0; i< 1000000; i++) {
+}
+console.timeEnd("How long does it take to count to a million")
